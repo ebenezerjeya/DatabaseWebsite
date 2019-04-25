c14ea14e91bfae37ad252fac080e5e99492d6fa3
marcustankw <44251972+marcustankw@users.noreply.github.com>
2019-04-25 00:14:18 -0500

    Add files via upload

--- View.php
+++ View.php
@@ -28,7 +28,7 @@
     }
     </style>
     <body>
-
+        
          <!-- Main navigation -->
         <div id="sidebar">
 
@@ -91,30 +91,7 @@
                                 <h4>Search Reminders</h4>
                                 <form class="form-inline my-2 my-lg-0">
                                     <input class="form-control mr-sm-2" type="text" name="ID" placeholder="User ID" aria-label="Search" maxlength="8">
-                                    <?php
-                      			include_once("db.php");
-					$IDinput=$_POST['ID']
-                      			$query = "select * from reminder where User_ID = $IDinput order by date";
-                            		$stmt = $db->prepare($query);
-                            		$stmt->execute();
-                      			$count = $stmt->rowCount();
-                      			$results = $stmt->fetchAll();
-                      			if ($count > 0) {
-                      			  foreach ($results as $row) {
-                      			  $id = $row['Reminder_ID'];
-                                          $uid = $row['User_ID'];
-                                          $title = $row['Reminder_title'];
-                                          $descrip = $row['Description'];
-                                          $date = $row['Date'];
-                                          $time = $row['Time'];
-                                          $all_day = $row['All_day'];
-                      			  echo '<option style="color: #000; font-weight: bold;" value="'.$uid.'">'.$title.'</option>';
-                      			  }
-                      			} else {
-                      				echo '<option value="0">User ID does not exist</option>';
-                      			}
-                      		  ?>
-                                    <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                                    <button class="btn btn-secondary my-2 my-sm-0" id="search" name="search" type="submit">Search</button>
                                 </form>
                             </div>
 
@@ -145,6 +122,42 @@
                 </div>
             </div>
         </div>
-
+        <!-- Bootcamp JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+        <script type="text/javascript" 
+            src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+        </script>
+        
+        <!-- Unsure of what to change, etc: "#search" and '#dept'  -->      
+        <script type="text/javascript">
+        $('#search').click(function(){
+            var d_id = $('#dept').find(":selected").val();
+            $.ajax({
+                type:'POST',
+                url:'find_user_ajax.php',
+                data:'d_id='+d_id,
+                success:function(html){
+                    $('#area_container').show();
+                    $('#area_container').html(html);  
+                }
+            }); 		
+        });
+            
+        $('#dept').change(function () {
+            var u_id = $('#dept').find(":selected").val();
+            $.ajax({
+                type:'POST',
+                url:'find_user_ajax.php',
+                data:'d_id='+d_id,
+                success:function(html){
+                    $('#area_container').show();
+                    $('#area_container').html(html);  
+                }
+            });
+        });				
+        </script>
     </body>
 </html>
