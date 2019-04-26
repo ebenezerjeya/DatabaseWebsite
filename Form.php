b0376894433224e385dbd8be598b9b5bc313a7d4
Ebenezer Jeya <ebenezer.jeya@gmail.com>
2019-04-25 20:58:43 -0500

    Add files via upload

--- Form.php
+++ Form.php
@@ -21,44 +21,39 @@
 
     <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        include_once("dbconnection_pdo.php");
-        $query = 'select * from `user` where User_ID = $(\'#userID\')';
-        $stmt = $DBH->prepare($query);
-        $stmt->bindParam(1, $_POST['i_id'], PDO::PARAM_STR);
+        include_once("db.php");
+        $query = "select * from `user` where User_id = ?";
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(1, $_POST['userID'], PDO::PARAM_STR);
         $stmt->execute();
         $count = $stmt->rowCount();
         if($count > 0) {
-            $query = "insert into reminder (User_id, reminder_title, description, `time`, `date`, all_day) values (?,?,?,?,?)";
-            $stmt = $DBH->prepare($query);
-            $stmt->bindParam(1, $_POST['User_id'], PDO::PARAM_STR);
-            $stmt->bindParam(2, $_POST['reminder_title'], PDO::PARAM_STR);
+            $query = "insert into reminder (User_id, reminder_title, description) values (?,?,?)";
+            $stmt = $db->prepare($query);
+            $stmt->bindParam(1, $_POST['userID'], PDO::PARAM_STR);
+            $stmt->bindParam(2, $_POST['reminderTitle'], PDO::PARAM_STR);
             $stmt->bindParam(3, $_POST['description'], PDO::PARAM_STR);
-            $stmt->bindParam(4, $_POST['`time`'], PDO::PARAM_STR);
-            $stmt->bindParam(5, $_POST['`date`'], PDO::PARAM_STR);
+            #mm "/" dd "/" y
+            #$stmt->bindParam(4, $_POST['`time`'], PDO::PARAM_STR);
+            #$stmt->bindParam(5, $_POST['`date`'], PDO::PARAM_STR);
             $stmt->execute();
             header('Location: find_instructor.php');
         } else {
             // insert a new instructor
-            $query = "select * from `user` where User_ID = ?";
-            $stmt = $DBH->prepare($query);
-            $stmt->bindParam(1, $_POST['i_dept'], PDO::PARAM_STR);
-            $stmt->execute();
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
-            $dname = $row['`User`'];
             $query = "insert into `user` (User_id, First_name, Last_name, email) values (?,?,?,?)";
-            $stmt = $DBH->prepare($query);
-            $stmt->bindParam(1, $_POST['User_id'], PDO::PARAM_STR);
-            $stmt->bindParam(2, $_POST['First_name'], PDO::PARAM_STR);
-            $stmt->bindParam(3, $_POST['Last_name'], PDO::PARAM_STR);
-            $stmt->bindParam(4, $_POST['email'], PDO::PARAM_STR);
-            $stmt->execute();
-            $query = "insert into reminder (User_id, reminder_title, description, `time`, `date`, all_day) values (?,?,?,?,?)";
             $stmt = $db->prepare($query);
-            $stmt->bindParam(1, $_POST['User_id'], PDO::PARAM_STR);
-            $stmt->bindParam(2, $_POST['reminder_title'], PDO::PARAM_STR);
-            $stmt->bindParam(3, $_POST['description'], PDO::PARAM_STR);
-            $stmt->bindParam(4, $_POST['`time`'], PDO::PARAM_STR);
-            $stmt->bindParam(5, $_POST['`date`'], PDO::PARAM_STR);
+            $stmt->bindParam(1, $_POST['userID'], PDO::PARAM_STR);
+            $stmt->bindParam(2, $_POST['firstName'], PDO::PARAM_STR);
+            $stmt->bindParam(3, $_POST['lastName'], PDO::PARAM_STR);
+            $stmt->bindParam(4, $_POST['exampleInputEmail'], PDO::PARAM_STR);
+            $stmt->execute();
+            $query = "insert into reminder (User_id, reminder_title, description) values (?,?,?)";
+            $stmt = $db->prepare($query);
+            $stmt->bindParam(1, $_POST['userID'], PDO::PARAM_STR);
+            $stmt->bindParam(2, $_POST['reminderTitle'], PDO::PARAM_STR);
+            $stmt->bindParam(3, $_POST['reminderDescription'], PDO::PARAM_STR);
+            #$stmt->bindParam(4, $_POST['`time`'], PDO::PARAM_STR);
+            #$stmt->bindParam(5, $_POST['`date`'], PDO::PARAM_STR);
             $stmt->execute();
             header('Location: View.php');
         }
@@ -90,36 +85,36 @@
                                     <form action="form.php" method="post">
                                     <div class="form-group">
                                         <label for="userID">User ID</label>
-                                        <input type="text" class="form-control" id="userID" maxlength="8">
+                                        <input type="text" class="form-control" id="userID" name="userID" maxlength="8">
                                     </div>
 
                                     <div class="form-group">
                                         <label for="firstName">First Name</label>
-                                        <input type="text" class="form-control" id="firstName">
+                                        <input type="text" class="form-control" name="firstName" id="firstName">
                                     </div>
 
                                     <div class="form-group">
                                         <label for="lastName">Last Name</label>
-                                        <input type="text" class="form-control" id="lastName">
+                                        <input type="text" class="form-control" name="lastName" id="lastName">
                                     </div>
 
                                     <div class="form-group">
                                         <label for="exampleInputEmail" >Email address</label>
-                                        <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp">
+                                        <input type="email" class="form-control" id="exampleInputEmail" name="exampleInputEmail" aria-describeDBHy="emailHelp">
                                         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                                     </div>
 
 
                                     <div class="form-group">
                                         <label for="reminderTitle">Reminder Title</label>
-                                         <textarea class="form-control" id="reminderTitle" rows="1" maxlength="35"></textarea>
+                                         <textarea class="form-control" id="reminderTitle" name="reminderTitle" rows="1" maxlength="35"></textarea>
                                     </div>
 
                                     <div class="form-group">
                                         <label for="reminderDescription">Description</label>
-                                        <textarea class="form-control" id="reminderDescription" rows="3" maxlength="100"></textarea>
+                                        <textarea class="form-control" id="reminderDescription" name="reminderDescription" rows="3" maxlength="100"></textarea>
                                     </div>
-                                    <button type="submit" onclick="submitData" class="btn btn-primary">Submit</button>
+                                        <button type="submit" onclick="submitData" class="btn btn-primary">Submit</button></form>
 
                                 </fieldset>
                                 
--- db.php
+++ db.php
@@ -1,6 +1,8 @@
 <?php
 try {
-	$db = new PDO('mysql:host=localhost;dbname= "test";charset=utf8mb4', 'BruceU', 'irondragonslayer');
+	$nyb=2;
+	$nyf =5;
+	$db = new PDO('mysql:host=localhost;dbname=test;charset=utf8mb4', 'BruceU', 'irondragonslayer');
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 } catch (PDOException $e) {
