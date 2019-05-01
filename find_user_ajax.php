3872b4b994360e733f339c0dbae67131b00d7b3c
marcustankw <44251972+marcustankw@users.noreply.github.com>
2019-04-30 23:03:32 -0500

    Add files via upload

--- find_user_ajax.php
+++ find_user_ajax.php
@@ -13,39 +13,46 @@
 		$stmt->bindParam(1, $id, PDO::PARAM_STR);
 		$stmt->execute();
 		$results = $stmt->fetchall();
+		echo "<br>";
 		echo '<div class="row">';
-		echo '<div class="col-1.5 text-middle"><h5><u>Reminder ID</u></h5></div>';
+		echo '<div class="col-2 text-middle"><h5><u>Reminder ID</u></h5></div>';
+		echo '<div class="col-1 text-left"><h5><u>Date</u></h5></div>';
+		echo '<div class="col-1 text-middle"><h5><u>Time</u></h5></div>';		
 		echo '<div class="col-2 text-middle"><h5><u>Reminder Title</u></h5></div>';
 		echo '<div class="col-3 text-middle"><h5><u>Description</u></h5></div>';
-		echo '<div class="col-1 text-middle"><h5><u>Time</u></h5></div>';
-    	echo '<div class="col-1 text-middle"><h5><u>Date</u></h5></div>';
 		echo '</div>';
+		
 		foreach ($results as $row) {
 			echo '<div class="row">';
-			echo '<div class="col-1 text-middle">'.$row['Reminder_ID'].'</div>';
+			echo '<div class="col-2 text-middle">'.$row['Reminder_ID'].'</div>';
+			echo '<div class="col-1 text-middle">'.$row['Date'].'</div>';
+			echo '<div class="col-1 text-middle">'.$row['Time'].'</div>';
 			echo '<div class="col-2 text-middle">'.$row['Reminder_title'].'</div>';
 			echo '<div class="col-3 text-middle">'.$row['Description'].'</div>';
-			echo '<div class="col-1 text-middle">'.$row['Time'].'</div>';
-			echo '<div class="col-1 text-middle">'.$row['Date'].'</div>';
 			echo '</div>';
 		}
+
+		echo "<br>";
+		echo "<br>";
+
 		$query = "select * from `user` where User_ID = ?";
 		$stmt = $db->prepare($query);
 		$stmt->bindParam(1, $id, PDO::PARAM_STR);
 		$stmt->execute();
 		$results = $stmt->fetchall();
 		echo '<div class="row">';
-		echo '<div class="col-1 text-middle"><h5><u>First Name</u></h5></div>';
-		echo '<div class="col-1 text-middle"><h5><u>Last Name</u></h5></div>';
-		echo '<div class="col-1 text-middle"><h5><u>Email</u></h5></div>';
+		echo '<div class="col-2 text-middle"><h5><u>First Name</u></h5></div>';
+		echo '<div class="col-2 text-middle"><h5><u>Last Name</u></h5></div>';
+		echo '<div class="col-2 text-middle"><h5><u>Email</u></h5></div>';
 		echo '</div>';
 		foreach ($results as $row) {
 			echo '<div class="row">';
-			echo '<div class="col-1 text-middle">'.$row['First_Name'].'</div>';
-			echo '<div class="col-1 text-middle">'.$row['Last_Name'].'</div>';
-			echo '<div class="col-1 text-middle">'.$row['Email'].'</div>';
+			echo '<div class="col-2 text-middle">'.$row['First_Name'].'</div>';
+			echo '<div class="col-2 text-middle">'.$row['Last_Name'].'</div>';
+			echo '<div class="col-2 text-middle">'.$row['Email'].'</div>';
 			echo '</div>';
 		}
+		echo "<br>";
 	}
 	else{
 		echo 'No Such ID';
